ed018ab072214accd77afac943c98fe42fe149ab
Jim Kang <jimkang@gmail.com>
2015-05-10 22:33:46 -0400

    Added animals, Star Wars, and Zelda monsters.

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 var jsonfile = require('jsonfile');
 
+// TODO: Lazy load.
 var srdMonsters = jsonfile.readFileSync(__dirname + '/data/srd-monsters.json');
 var lovecraftMonsters = jsonfile.readFileSync(__dirname + '/data/lovecraft.json');
 var pokemon = jsonfile.readFileSync(__dirname + '/data/pokemon.json');
+var starWars = jsonfile.readFileSync(__dirname + '/data/star-wars.json');
+var zelda = jsonfile.readFileSync(__dirname + '/data/zelda.json');
+var animals = jsonfile.readFileSync(__dirname + '/data/animals.json');
 
 module.exports = {
   srd: srdMonsters,
   lovecraft: lovecraftMonsters,
-  pokemon: pokemon
+  pokemon: pokemon,
+  starWars: starWars,
+  zelda: zelda,
+  animals: animals
 };
